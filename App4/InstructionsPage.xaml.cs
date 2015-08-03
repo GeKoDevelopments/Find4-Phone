c118b180ca79c8223dd522d2984f03d24ca025ca
Giorgos Ath <geotha1995@hotmail.com>
2015-08-04 00:13:52 +0300

    Etoimo

---
+++ App4/InstructionsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Find4
+{
+    public sealed partial class InstructionsPage : Page
+    {
+        public InstructionsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.GoBack();
+            }
+        }
+    }
+}
